bc88bdcc701ca60babb11f554fb02b65e0dcedf5
OmarOchoaL <80289458+OmarOchoaL@users.noreply.github.com>
2021-06-24 00:01:15 -0600

    Add files via upload

---
+++ Archivos/src/Principal.java
@@ -0,0 +1,19 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Principal extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override
+    public void start(Stage stage) throws Exception {
+        Ventana ventana=new Ventana(stage);
+        Scene scene=new Scene(ventana.crearVentana());
+        stage.setWidth(900);
+        stage.setHeight(600);
+        stage.setTitle("ARCHIVOS");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
